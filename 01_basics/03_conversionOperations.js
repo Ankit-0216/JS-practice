6db1ce038f99f8cf7569e71bcf4448990ca235e1
Ankit-0216 <125597650+Ankit-0216@users.noreply.github.com>
2024-06-02 19:54:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "10" => 10
+// "10abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = "sada"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "something" => true
+
+let someNumber = 10
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
