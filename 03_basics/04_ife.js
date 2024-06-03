0a00898dfcf99728b9be6beec61dc7cab7807490
Ankit-0216 <125597650+Ankit-0216@users.noreply.github.com>
2024-06-03 20:54:40 +0000

    immediately invoked function

---
+++ 03_basics/04_ife.js
@@ -0,0 +1,11 @@
+// Immediately invoked function
+
+(function one() {
+    console.log("DB connected");
+})();
+
+// (function defination)(function execution)
+
+((num) => {
+    console.log(`DB Connected ${num}`);
+})("two");
