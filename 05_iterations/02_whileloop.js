b3c9008241ac22cd92ae4af5aa4d00180ffa4403
Ankit-0216 <125597650+Ankit-0216@users.noreply.github.com>
2024-06-14 05:48:33 +0000

    for , while and doWhile loop

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,21 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["a", "b", "c", "d"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 1
+
+do {
+   console.log(`Score is ${score}`);
+   score++ 
+} while (score < 7)
